47a4aeec28c87ad836928d02f82b3fc60c920552
parisa hashemi <phashemi775@gmail.com>
2024-04-18 11:20:13 +0330

    questions

--- m1/main.py
+++ m1/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from datetime import datetime
+from starlette.requests import Request
 
 app = FastAPI()
 
@@ -11,5 +13,18 @@ def calculate_factorial(number: int) -> int:
 async def calculate_factorial_endpoint(number_input: int):
     factorial_result = calculate_factorial(number_input)
     return {"number": number_input, "factorial": factorial_result}
+
+def get_client_ip(request: Request) -> str:
+    return request.client.host
+
+@app.get("/whoami")
+async def whoami(request: Request):
+    client_ip = get_client_ip(request)
+    current_time = datetime.utcnow().isoformat()
+    return {"client": client_ip, "time": current_time}
+
+
+
+
   
    
